22da03046c2f64bd92d088b3df58bbc57746ab54
Guy Allard <gallard@oldpete.anv.com>
2008-01-20 16:35:39 -0500

    Change mothership redeploy script

--- rdeploy.sh
+++ rdeploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd $1
-tar xzvf $2
+set -x
+gr=/opt/gitroot
+#
+cd $gr
+rm -rf suba.bare supa
+tar xzvf ~/suba.bare.tar.gz
+mv suba.bare suba
+#
+rm -rf supa.bare supa
+tar xzvf ~/supa.bare.tar.gz
+mv supa.bare supa
+#
+set +x
+
