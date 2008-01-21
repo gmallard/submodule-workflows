9d64c19f0317a1917efca91a14d293b85e47e2d0
Guy Allard <gallard@oldpete.anv.com>
2008-01-21 10:01:43 -0500

    Correct permissions in cloned repos.

--- gen.shared.sh
+++ gen.shared.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wd=/home/gallard/gw
+mkdir $wd
 cd $wd
 ruser=gallard@jeb
 rgr=/opt/gitroot
@@ -9,7 +10,8 @@ do
 	rm -rf $m
 	mkdir $m
 	cd $m
-	git init --shared=group
+	# git init --shared=group
+	git init
 	echo "#" >.gitignore
 	git add .gitignore
 	git commit -m "First ignore"
@@ -17,6 +19,8 @@ do
 	rm -rf $m.bare
 	git clone --bare $m $m.bare
 	touch $m.bare/git-daemon-export-ok
+	find $m.bare -type d -exec chmod 2775 {} \;
+	find $m.bare -type f -exec chmod 644 {} \;
 	rm $m.bare.tar.gz
 	tar czvf $m.bare.tar.gz $m.bare
 	#
--- rdeploy.sh
+++ rdeploy.sh
@@ -7,14 +7,10 @@ cd $gr
 rm -rf suba.bare suba
 tar xzvf /home/gallard/suba.bare.tar.gz
 mv suba.bare suba
-find suba -type d -exec chmod 2775 {} \;
-find suba -type f -exec chmod 664 {} \;
-#
+
 rm -rf supa.bare supa
 tar xzvf /home/gallard/supa.bare.tar.gz
 mv supa.bare supa
-find supa -type d -exec chmod 2775 {} \;
-find supa -type f -exec chmod 664 {} \;
 #
 set +x
 
